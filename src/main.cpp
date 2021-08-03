9122b3f80230a44439954b9081c8b8e8d2bc91eb
Vlad Hategan <vlad.hategan@telenav.com>
2021-08-03 11:50:25 +0300

    Added initial CMakeLists file

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,8 @@
 
 #include "vlogger.h"
 
-#include <iostream>
-#include <tuple>
-#include <vector>
-
-
-
 int main()
 {
-    std::tuple<std::string, std::string> t = std::make_tuple("kek1","kek2");
 
     vlogger::VLogger vlogger;
 
